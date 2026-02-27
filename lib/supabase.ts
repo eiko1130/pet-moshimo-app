a23d21d4db3bd6be977d472a11ad2665dfa81010
eiko1130 <nancoco.net@icloud.com>
2026-02-27 17:23:07 +0900

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://nukpisixfolbnzkvorym.supabase.co'
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'sb_publishable_Q1XSJAMEDyMf8-sZw06UMA_MlsGKnAC'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
